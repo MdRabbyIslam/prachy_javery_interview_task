6f6000f6017a5b76006f16a438373e6718b4e53f
Rabby Sopno <sopno01737@gmail.com>
2020-12-03 19:23:21 +0600

    complete without task 3

--- task3.js
+++ task3.js
@@ -1,16 +1,5 @@
-let str1 = "geeke";
-let str2 = "eke";
-
-function lcs(str1, str2, m, n) {
-  if (m == 0 || n == 0) {
-    return 0;
-  }
-
-  if (str1[m - 1] == str2[n - 1]) {
-    return 1 + lcs(str1, str2, m - 1, n - 1);
-  } else {
-    return Math.max(lcs(str1, str2, m, n - 1), lcs(str1, str2, m - 1, n));
-  }
-}
-
-console.log(lcs(str1, str2, str1.length, str2.length));
+// I learn SCS and LCS of dynamic programming
+// but I dont know any language without javaScript
+// so that I can not make the table of top down for scs
+// sorry for not done . but the concept is very clear, I can explain all this thing.
+// but i will not be able to apply that in my javaScript.
